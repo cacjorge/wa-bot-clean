8d14b2f4a12ddfc47a68eb71047d6767fd368c87
iuha <cacjorge@gmail.com>
2021-01-10 23:37:53 -0300

    Create ud.js

---
+++ commands/ud.js
@@ -0,0 +1,15 @@
+const ud = require('urban-dictionary')
+
+exports.run = async (bot, message, args) => {
+	ud.term(args[1]).then((result) => {
+						const entries = result.entries;
+						bot.reply(message.from,`*Termo:* ${entries[0].word}\n *Definição:* ${entries[0].definition}\n *Exemplo:* ${entries[0].example}\n`);
+					})
+};
+
+exports.help = {
+    name: "Urban Dictionary",
+    description: "Search a term on Urban Dictionary",
+    usage: "ud <term>",
+    cooldown: 5
+};
