50d185a9f89a97420f4dd617f9259930ae5f9b8b
iuha <cacjorge@gmail.com>
2021-01-10 23:43:26 -0300

    Update ud.js

--- commands/ud.js
+++ commands/ud.js
@@ -1,10 +1,11 @@
-const ud = require('urban-dictionary')
+const ud = require('urban-dictionary');
 
 exports.run = async (bot, message, args) => {
-	ud.term(args[1]).then((result) => {
-						const entries = result.entries;
-						bot.reply(message.from,`*Termo:* ${entries[0].word}\n *Definição:* ${entries[0].definition}\n *Exemplo:* ${entries[0].example}\n`);
-					})
+	ud.term(args[1])
+		.then((result) => {
+			const entries = result.entries;
+			bot.sendText(message.from,`*Termo:* ${entries[0].word}\n *Definição:* ${entries[0].definition}\n *Exemplo:* ${entries[0].example}\n`);
+		})
 };
 
 exports.help = {
