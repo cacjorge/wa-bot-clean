a982e47ba1872db96e1b3f6199462ef54bb83c37
iuha <cacjorge@gmail.com>
2021-01-19 23:59:46 -0300

    Update joke.js

--- commands/joke.js
+++ commands/joke.js
@@ -4,7 +4,7 @@ exports.run = async (bot, message, args) => {
 	console.log(args);
 	const joke = await axios.get('https://v2.jokeapi.dev/joke/Any');
 	let jokeText = joke.joke;
-	if(joke.type === 'twopart') const jokeText = joke.setup + '\n' + joke.delivery;
+	if(joke.type === 'twopart') jokeText = joke.setup + '\n' + joke.delivery;
 	bot.reply(message.from, jokeText , message.id)
 };
 
